baa2d03cbdc01c1db7b02fec5eb8c21af5b30917
abderrahman504 <rahmankhsherb@gmail.com>
2024-11-13 17:34:41 +0200

    testing cmake with a simple main.cpp main.cpp simply creates an instance of State and Automata.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
-
+#include "State.h"
+#include "Automata.h"
 
 
 int main(char argc[], int argv) {
+	State state = State("state", true, 1, false);
+	Automata automata = Automata(&state);
 	return 0;
 }
