6486fe0fcfc103eb484f52e737bdc27e29a595fd
abderrahman504 <rahmankhsherb@gmail.com>
2024-11-13 15:03:07 +0200

    Implement State class

---
+++ src/State.h
@@ -0,0 +1,60 @@
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+
+using namespace std;
+
+class State
+{
+	
+	private:
+
+	string name;
+	bool is_acceptor;
+	int acceptor_priority;
+	unordered_map<char, vector<State*>*> transitions;
+	bool is_null;
+
+
+	public:
+
+	State(string name, bool is_acceptor, int acceptor_priority, bool is_null){
+		this->name = name;
+		this->is_acceptor = is_acceptor;
+		this->acceptor_priority = acceptor_priority;
+		this->is_null = is_null;
+		transitions = unordered_map<char, vector<State*>*>();
+	}
+
+	~State(){
+		// delete each vector in the transitions map
+		for (auto it = transitions.begin(); it != transitions.end(); ++it){
+			delete it->second;
+		}
+	}
+
+	void addTransition(char input, vector<State*>* next_states){
+		// If this input isn't in the transitions map, add it
+		if(transitions.find(input) == transitions.end()){
+			transitions.insert({input, new vector<State*>});
+		}
+		// Add each state in next_states to transitions map
+		for (auto next_stat : *next_states){
+			transitions[input]->push_back(next_stat);
+		}
+	}
+
+	vector<State*>* getTransitions(char input){
+		if(transitions.find(input) == transitions.end()){
+			return nullptr;
+		}
+		else return transitions[input];
+	}
+
+	bool isAcceptor(){return is_acceptor;}
+
+	int getAcceptorPriority(){return acceptor_priority;}
+
+	bool isNull(){return is_null;}
+};
