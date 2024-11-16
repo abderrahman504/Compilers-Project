54f7f8a5183a4694d933bfc0415fe2820bcf5dd5
abderrahman504 <rahmankhsherb@gmail.com>
2024-11-16 20:11:53 +0200

    Remove isNull() and add addEpsilonTransition() to `State` Null states don't need to be represented by a state object. The automata is considered to be in a null state if its current state is an empty vector.

--- src/State.h
+++ src/State.h
@@ -16,24 +16,32 @@ class State
 	bool is_acceptor;
 	int acceptor_priority;
 	unordered_map<char, vector<State*>*> transitions;
-	bool is_null;
 
 
 	public:
 
-	State(string name, bool is_acceptor, int acceptor_priority, bool is_null);
+	State(string name, bool is_acceptor, int acceptor_priority);
 
 	~State();
 
+	// Adds a number of transitions from this state to `next_states` when `input` is provided
 	void addTransition(char input, vector<State*>* next_states);
 
+	// Adds a number of transitions from this state to `next_states` when no input is provided
+	void addEpsilonTransition(vector<State*>* next_states);
+
+	// Gets all states that this state moves to when `input` is provided
 	vector<State*>* getTransitions(char input);
 
+	// Returns true if this state is an acceptor state
 	bool isAcceptor();
 
+	// Returns the acceptor priority of this state
 	int getAcceptorPriority();
 
-	bool isNull();
+	// Returns all transitions from this state
+	unordered_map<char, vector<State*>*> getTransitions();
+
 };
 
 #endif
