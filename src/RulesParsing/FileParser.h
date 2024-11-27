20d50fe280f7cd57bc8c6afada5f660a232a3294
abderrahman504 <rahmankhsherb@gmail.com>
2024-11-27 13:02:53 +0200

    Change signature of FileParser.getRegularDefinitions Also changed the private methods.

--- src/RulesParsing/FileParser.h
+++ src/RulesParsing/FileParser.h
@@ -7,13 +7,13 @@
 
 class FileParser {
 private:
-    std::unordered_map<std::string, std::vector<std::string>> regularDefinitions; // Stores LHS = RHS
+    std::unordered_map<std::string, std::string> regularDefinitions; // Stores LHS = RHS
     std::unordered_map<std::string, std::string> regularExpressions; // Stores LHS: RHS
     std::vector<std::string> keywords;                               // List of keywords
     std::vector<std::string> punctuations;                           // List of operators
 
-    std::vector<std::string> processDefinitionRhs(std::string rhs);  // Turns the RHS of a definition into a vector of all strings recongized as the definition
-    std::string processExpressionRhs(std::string rhs);  // Expands any definitions found in the RHS of an expression.
+    std::string expandRhs(std::string rhs);  // Expands any character ranges like a-z, 0-9 etc, and substitutes any definition references.
+    std::string substituteDefinitions(std::string rhs); // Replaces any definitions referenced in `rhs` with the the definition's RHS.
 
 public:
     FileParser() = default;
@@ -22,7 +22,7 @@ public:
     void parseFile(const std::string& filePath);
 
     // Getters for parsed data
-    const std::unordered_map<std::string, std::vector<std::string>>& getRegularDefinitions() const;
+    const std::unordered_map<std::string, std::string>& getRegularDefinitions() const;
     const std::unordered_map<std::string, std::string>& getRegularExpressions() const;
     const std::vector<std::string>& getKeywords() const;
     const std::vector<std::string>& getPunctuations() const;
