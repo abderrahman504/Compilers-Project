22b14981adb4ed3948007223db3bbd241a7beea6
abderrahman504 <rahmankhsherb@gmail.com>
2024-12-01 13:47:27 +0200

    Moved input scanning functions from `Automata` to `Scanner`

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -8,137 +8,65 @@ Automata::Automata(State* initial_state){
 	this->initial_state = initial_state;
 	current_states = unordered_set<State*>();
 	reset();
-	last_acceptor_idx = -1;
-	acceptor_encountered = false;
-	input_stream = nullptr;
 }
 
-Automata::~Automata(){
-	if(input_stream != nullptr) delete[] input_stream;
-}
+Automata::~Automata(){}
 
 
 void Automata::reset(){
 	current_states.clear();
 	current_states.insert(initial_state);
 	// If there are epsilon transitions from the initial state then add them to the current state.
-	vector<State*> epsi_trans = initial_state->getEpsilonTransitions();
-	for(auto state : epsi_trans){
-		current_states.insert(state);
-	}
-}
-
-
-void Automata::setInput(char* input_stream, int size){
-	if(input_stream != nullptr) delete[] input_stream;
-	this->input_stream = new char[size];
-	this->input_size = size;
-	for (int i = 0; i < size; i++){
-		this->input_stream[i] = input_stream[i];
-	}
-	input_idx = 0;	
-	acceptor_encountered = false;
-	last_acceptor = unordered_set<State*>();
-}
-
-
-string Automata::nextToken(){
-	acceptor_encountered = false;
-
-	vector<char> chars = vector<char>();
-	int local_acceptor_idx = -1;
-
-	// Keep processing input until the eof or null state is reached
-	// Null state is represented by an empty current_states set
-	while(input_idx < input_size && current_states.size() != 0){
-		char c = input_stream[input_idx];
-		unordered_set<State*> next_states = unordered_set<State*>();
-		// Transition from current states to next states using c as input
+	bool states_added = true;
+	while (states_added){
+		states_added = false;
 		for (auto state : current_states){
-			vector<State*> next = state->getTransitions(c);
-			for (auto next_state : next){
-				next_states.insert(next_state);
+			vector<State*> epsi_trans = state->getEpsilonTransitions();
+			for (auto next : epsi_trans){
+				if(current_states.find(next) == current_states.end()){
+					states_added = true;
+					current_states.insert(next);
+				}
 			}
 		}
-		// If any epsilon transitions exist from a state in next_states then add them to next_states
+	}
+}
+
+void Automata::consume(char c)
+{
+	unordered_set<State*> next_states = unordered_set<State*>();
+	// Transition from current states to next states using c as input
+	for (auto state : current_states){
+		vector<State*> next = state->getTransitions(c);
+		for (auto next_state : next){
+			next_states.insert(next_state);
+		}
+	}
+	// If any epsilon transitions exist from a state in next_states then add them to next_states
+	bool states_added = true;
+	while (states_added){
+		states_added = false;
 		for (auto state : next_states){
 			vector<State*> epsi_trans = state->getEpsilonTransitions();
-			for (auto next_state : epsi_trans){
-				next_states.insert(next_state);
-			}
-		}
-		
-		// Check if next_states includes acceptors and update last_acceptor and last_acceptor_idx
-		for (auto state : next_states){
-			if(state->isAcceptor()){
-				acceptor_encountered = true;
-				last_acceptor = next_states;
-				last_acceptor_idx = input_idx;
-				local_acceptor_idx = chars.size();
-				break;
-			}
-		}
-
-		chars.push_back(c);
-		input_idx++;
-		current_states = next_states;
-	}
-	
-	// Create a token out of chars
-	auto token_end = acceptor_encountered ? chars.begin()+local_acceptor_idx+1 : chars.end(); 
-	string token = string(chars.begin(), token_end);
-	reset();
-	input_idx = last_acceptor_idx + 1;
-	return token;
-
-}
-
-
-State* Automata::getLastAcceptor(){
-	if(!acceptor_encountered) return nullptr;
-
-	State* acceptor = nullptr;
-	int priority = 0;
-	for (auto state : last_acceptor){
-		if(state->isAcceptor()){
-			if(acceptor == nullptr || state->getAcceptorPriority() > priority){
-				acceptor = state;
-				priority = state->getAcceptorPriority();
+			for (auto next : epsi_trans){
+				if(next_states.find(next) == next_states.end()){
+					states_added = true;
+					next_states.insert(next);
+				}
 			}
 		}
 	}
-	return acceptor;
+	current_states = next_states;
 }
 
-
-int Automata::getLastTokenEnd(){
-	return last_acceptor_idx;
-}
 
 unordered_set<State*> Automata::getCurrentState(){
 	return current_states;
 }
 
 
-
-bool Automata::isFinished(){
-	return input_idx == input_size;
-}
 State* Automata::getInitialState() const { return initial_state; }
 
-
-void Automata::merge(const Automata& other) {
-	// Abdelrahman's suggestion for merge implementation
-	State* new_initial = new State("", false, 0);
-	vector<State*> vec = vector<State*>{initial_state, other.initial_state};
-	new_initial->addEpsilonTransition(vec);
-	initial_state = new_initial;
-
-}
-
-void Automata::setLastAcceptor(State* acceptor) {
-	last_acceptor = std::unordered_set<State*>{acceptor};
-}
 
 unordered_set<State*> Automata::getAllStates() const{
     unordered_set<State*> visited; // To keep track of visited states
--- src/Scanner.h
+++ src/Scanner.h
@@ -8,12 +8,12 @@ using namespace std;
 
 class Scanner
 {
-
-	unordered_set<State*> last_acceptor; // Stores the last acceptor state(s) encountered since the last call to nextToken().
+	Automata automata = Automata(NULL); // The automata used for scanning input.
+	State* last_acceptor; // Stores the last acceptor state(s) encountered since the last call to nextToken().
 	int last_acceptor_idx; // The index in the input stream where the last acceptor state was encountered.
 	bool acceptor_encountered; // whether an acceptor state has been encountered since the last call to nextToken().
 	int input_idx; // Tracks the progress of the automata through the input stream.
-	char* input_stream; // The input stream to read from.
+	string input_stream; // The input stream to read from.
 	int input_size; // Size of the input stream.
 
 	public:
@@ -23,7 +23,7 @@ class Scanner
 	void setInput(string input);
 
 	// Returns the next valid token in the input stream, or the rest of the stream if no valid token was found
-	string nextToken(bool skip_whitespace = true);
+	string nextToken();
 	
 	// Use after `nextToken()` to get the type of the last valid token, or an empty string if the last token was invalid.
 	string getLastTokenType();
