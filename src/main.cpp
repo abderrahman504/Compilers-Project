4f25a56ded4b70debf8c7bf5ab9a72fe23e5f20f
abderrahman504 <rahmankhsherb@gmail.com>
2024-12-01 13:04:33 +0200

    main tests the NfaBuilder

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include "State.h"
 #include "Automata.h"
-#include "NfaToDfaTest.cpp"
-
+#include "NfaBuilder.h"
+#include "RulesParsing/FileParser.h"
 
 int main(int argv, char **argc) {
-	nfa_to_dfa_test();
+	FileParser parser;
+	parser.parseFile("example_rules.txt");
+	NFABuilder nfaBuilder;
+	Automata nfa = nfaBuilder.getFullNFA(parser.getRegularExpressions(), parser.getKeywords(), parser.getPunctuations());
+	
 	// print
 	return 0;
 }
